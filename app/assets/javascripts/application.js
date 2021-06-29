3544e5684dcc39e9dd986552168e2f6ead44b0d9
maryoryo <liyomaxxde@gmail.com>
2021-06-29 15:43:28 +0000

    [create]logo-feedin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,14 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+/*global $*/
+
+$(function() {
+	setTimeout(function(){
+		$('.logo_fadein p').fadeIn(1000);
+	},500);
+	setTimeout(function(){
+		$('.logo_fadein').fadeOut(1000);
+	},2500);
+});
